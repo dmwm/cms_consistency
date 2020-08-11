e96ce37d824b8796f7012ce7bca8cfefedbcede9
Igor Mandrichenko <ivm@fnal.gov>
2020-08-11 19:01:47 +0200

    .

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 if [ "$1" == "" ]; then
-	echo 'Usage: run_site_cmp3.sh <config file> <RSE name> <output dir> <cert> <key>'
+	echo 'Usage: run_site_cmp3.sh <config file> <rucio.cfg> <RSE name> <output dir> <cert> <key>'
 	exit 2
 fi
 
+do_run="yes"
+
+if [ "$1" == "shell" ]; then
+	do_run="no"
+	shift
+fi
+
 config_file=$1
-RSE=$2
-output=$3
-cert=$4
-key=$5
+rucio_config_file=$2
+RSE=$3
+output=$4
+cert=$5
+key=$6
 
 mkdir -p ${output}
 if [ ! -d ${output} ]; then
@@ -18,15 +26,27 @@ if [ ! -d ${output} ]; then
 fi
 
 scratch=/tmp/${USER}/cms_recon
+mkdir -p $scratch
+if [ ! -d ${scratch} ]; then
+        echo Scartch must be a directory
+        exit 1
+fi
+
 cfg_dir=${scratch}
 
 mkdir -p $output
 mkdir -p $cfg_dir
 chmod go-rwx $cfg_dir
 cp $config_file ${cfg_dir}/config.yaml
+cp $rucio_config_file ${cfg_dir}/rucio.cfg
 cp $cert ${cfg_dir}/cert
 cp $key ${cfg_dir}/key
 chmod go-rwx ${cfg_dir}/*
 
-docker run --rm -v ${cfg_dir}:/config -v ${output}:/output cms-recon \
-	./run.sh /config/config.yaml ${RSE} /output /config/cert /config/key
+if [ "$do_run" == "yes" ]; then
+	docker run --rm -v ${cfg_dir}:/config -v ${output}:/output cms-recon \
+	   ./run.sh /config ${RSE} /output /config/cert /config/key
+else
+	echo would run: ./run.sh /config ${RSE} /output /config/cert /config/key
+	docker run -ti --rm -v ${cfg_dir}:/config -v ${output}:/output cms-recon /bin/bash
+fi	
