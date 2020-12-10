a537033821600a430ac20fd65693c8296966c8c5
Igor Mandrichenko <igorvm@gmail.com>
2020-12-10 17:14:03 -0600

    .

--- monitor/server/run.sh
+++ monitor/server/run.sh
@@ -4,14 +4,14 @@ prefix=cmscon
 app=monitor
 
 if [ "$1" == "shell" ]; then
-	docker run --rm --name appsrv-$app \
+	docker run --rm --name ${prefix}-$app \
 		-ti \
 		-v /storage/local/data1/ivm/logs:/home/appsrv/logs \
 		-p 9093:9093 \
 		${prefix}-$app /bin/bash
 
 else
-	docker run --rm  --name appsrv-$1 \
+	docker run --rm  --name ${prefix}-$1 \
 		-d \
 		-v /storage/local/data1/ivm/logs:/home/appsrv/logs \
 		-p 9093:9093 \
