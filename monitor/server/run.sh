14b8becf963e228176d84e2a1330bffa91db5662
Igor Mandrichenko <igorvm@gmail.com>
2020-12-10 17:19:56 -0600

    .

--- monitor/server/run.sh
+++ monitor/server/run.sh
@@ -6,15 +6,15 @@ app=monitor
 if [ "$1" == "shell" ]; then
 	docker run --rm --name ${prefix}-$app \
 		-ti \
-		-v /storage/local/data1/ivm/logs:/home/appsrv/logs \
-		-p 9093:9093 \
+		-v `pwd`/real-samples:/samples \
+		-p 8888:9093 \
 		${prefix}-$app /bin/bash
 
 else
 	docker run --rm  --name ${prefix}-$1 \
 		-d \
-		-v /storage/local/data1/ivm/logs:/home/appsrv/logs \
-		-p 9093:9093 \
+		-v `pwd`/real-samples:/samples \
+		-p 8888:9093 \
 		${prefix}-$app
 fi
 
