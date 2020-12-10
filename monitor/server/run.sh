5d212b9254ff1cb762f25eb2a919ad251f4de8d2
Igor Mandrichenko <igorvm@gmail.com>
2020-12-10 17:21:01 -0600

    .

--- monitor/server/run.sh
+++ monitor/server/run.sh
@@ -7,14 +7,14 @@ if [ "$1" == "shell" ]; then
 	docker run --rm --name ${prefix}-$app \
 		-ti \
 		-v `pwd`/real-samples:/samples \
-		-p 8888:9093 \
+		-p 8888:8888 \
 		${prefix}-$app /bin/bash
 
 else
 	docker run --rm  --name ${prefix}-$1 \
 		-d \
 		-v `pwd`/real-samples:/samples \
-		-p 8888:9093 \
+		-p 8888:8888 \
 		${prefix}-$app
 fi
 
