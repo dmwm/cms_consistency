0bb8563c6a1447f0e654160d02bb23830ee49e86
Igor Vasilyevich Mandrichenko <imandric@lxplus725.cern.ch>
2020-12-15 23:48:22 +0100

    .

--- monitor/cmsweb/create_secret.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl -n default create secret generic consistency-monitor-cephfs-secret \
-    --from-literal=userKey=AQBNGW5fePrMDhAAmZJpoeZ40VjODlprwyMjMg== \
-    --from-literal=userID=estid
