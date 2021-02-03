d8be04666fed270f61a350ecc841e6ecb3cdc34b
Igor Mandrichenko <ivm@fnal.gov>
2021-02-03 10:13:04 -0600

    .

--- monitor/server/start.sh
+++ monitor/server/start.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-echo start.sh: version 1
+echo start.sh: version 2
 
-CC_DATA=/cc_data
-WM_DATA=/wm_data
+CC_DATA=/reports
+WM_DATA=/reports/unmerged
 
 cd ~
 echo "--- starting ---"
 ls -l
 ls -l app
-ls -ld $DATA
+
+echo "--- $CC_DATA: ---"
+ls -ld $CC_DATA
+ls -l $CC_DATA
+
+echo "--- $WM_DATA: ---"
+ls -ld $WM_DATA
+ls -l $WM_DATA
+
 python -V
+
 echo "--- starting server with: " python app/server.py "$@" 8400 $CC_DATA $WM_DATA
 python app/server.py "$@" 8400 $CC_DATA $WM_DATA
