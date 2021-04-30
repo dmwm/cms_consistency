dd2f4b7ecbea18c11a2a4377d598bc516da4dfa1
Igor Mandrichenko <igorvm@gmail.com>
2021-04-30 08:49:27 -0400

    .

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -3,7 +3,7 @@ import sys, glob, json, time, os, gzip
 from datetime import datetime
 from wm_handler import WMHandler, WMDataSource
 
-Version = "1.4.2"
+Version = "1.4.3"
 
 class DataViewer(object):
     
