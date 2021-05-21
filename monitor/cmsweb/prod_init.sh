561dd72d9bdf688585f5fa960094e615f229bf68
Igor Mandrichenko <ivm@fnal.gov>
2021-05-21 16:23:16 +0200

    .

--- monitor/cmsweb/prod_init.sh
+++ monitor/cmsweb/prod_init.sh
@@ -1,5 +1,12 @@
 cd ~
-wget https://cernbox.cern.ch/index.php/s/GRiyu10zwWHlOKc/download -O config.cmsweb-k8s-services-prod
+
+wget https://cernbox.cern.ch/index.php/s/M4UtMwA2IxJhvx6/download -O config.cmsweb-k8s-services-prodnew
 export OS_TOKEN=$(openstack token issue -c id -f value)
-export KUBECONFIG=$PWD/config.cmsweb-k8s-services-prod
+export KUBECONFIG=$PWD/config.cmsweb-k8s-services-prodnew
 cd -
+
+
+
+#wget https://cernbox.cern.ch/index.php/s/GRiyu10zwWHlOKc/download -O config.cmsweb-k8s-services-prod
+#export OS_TOKEN=$(openstack token issue -c id -f value)
+#export KUBECONFIG=$PWD/config.cmsweb-k8s-services-prod
--- monitor/cmsweb/testbed_init.sh
+++ monitor/cmsweb/testbed_init.sh
@@ -1,5 +1,5 @@
 cd $HOME
-wget https://cernbox.cern.ch/index.php/s/Fnxcj4x3sUm92cs/download -O config.cmsweb-k8s-services-testbed
+wget https://cernbox.cern.ch/index.php/s/o4pP0BKhNdbPhCv/download -O config.cmsweb-testbed
 export OS_TOKEN=$(openstack token issue -c id -f value)
-export KUBECONFIG=$PWD/config.cmsweb-k8s-services-testbed
+export KUBECONFIG=$PWD/config.cmsweb-testbed
 cd -
