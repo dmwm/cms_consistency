1a12bbc29e963c2eb4c991c62139d12407ad48c1
Igor Mandrichenko <igorvm@gmail.com>
2021-07-12 11:43:43 -0500

    .

--- monitor/server/app/wm_handler.py
+++ monitor/server/app/wm_handler.py
@@ -85,8 +85,8 @@ class WMDataSource(object):
         return self.convert_rse_item(data)
 
     def ls(self, rse=None):
-        #pattern = f"{self.Path}/*_stats.json" if rse is None else f"{self.Path}/{rse}_stats.json"
-        files = glob.glob(f"{self.Path}/*_stats.json")
+        pattern = f"{self.Path}/*_stats.json" if rse is None else f"{self.Path}/{rse}_stats.json"
+        files = glob.glob(pattern)
         out = []
         for path in files:
             d = { "path": path, "error":"",
