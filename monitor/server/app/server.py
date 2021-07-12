eef51c26712ce04a5ba46ac14a0c5dc81a1f24a9
Igor Mandrichenko <igorvm@gmail.com>
2021-07-12 13:01:36 -0500

    v 1.4.11

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -3,7 +3,7 @@ import sys, glob, json, time, os, gzip
 from datetime import datetime
 from wm_handler import WMHandler, WMDataSource
 
-Version = "1.4.10"
+Version = "1.4.11"
 
 class DataViewer(object):
     
