8625dd8be72b2de74aa1d84cc792c8f45cb7a019
Igor Mandrichenko <igorvm@gmail.com>
2021-07-28 13:44:00 -0500

    .

--- monitor/server/app/wm_handler.py
+++ monitor/server/app/wm_handler.py
@@ -105,12 +105,12 @@ class WMDataSource(object):
                     break
         return rse_stats
         
-    def stats(self):
+    def latest_stat_per_rse(self):
         data = self.latest_stats()
         stats = { rse_info["rse"]:self.convert_rse_item(rse_info) for rse_info in data }
         return stats
         
-    def stats_for_rse(self, rse):
+    def latest_stats_for_rse(self, rse):
         files = sorted(glob.glob(f"{self.Path}/{rse}_*_stats.json"))
         latest_file = None
         for path in files:
@@ -127,6 +127,24 @@ class WMDataSource(object):
             return self.convert_rse_item(data)
         else:
             return None
+            
+    def all_stats_for_rse(self, rse):
+        out = []
+        files = sorted(glob.glob(f"{self.Path}/{rse}_*_stats.json"))
+        for path in files:
+            _, run = self.parse_stats_path(path)
+            try:
+                data = json.loads(open(path, "r").read())["scanner"]
+            except KeyError:
+                data = {    "error":    f"scanner section not found in {path}"   }
+            except:
+                data = {    "error":    f"JSON parse error in {path}"   }
+            else:
+                data = self.convert_rse_item(data)
+            data["run"] = run
+            out.append(data)
+        out = sorted(out, key=lambda d:d["run"])
+        return out
 
     def ls(self, rse=None):
         pattern = f"{self.Path}/*_stats.json" if rse is None else f"{self.Path}/{rse}_*_stats.json"
@@ -163,7 +181,7 @@ class WMHandler(WPHandler):
 
     def stats(self, request, replapth, **args):
         ds = self.App.WMDataSource
-        data = ds.stats()
+        data = ds.latest_stat_per_rse()
         return json.dumps(data), "text/json" 
     
     def read_file(self, f):
@@ -208,25 +226,37 @@ class WMHandler(WPHandler):
                 return self.json_iterator(ds.file_list_as_iterable(rse)), headers
         except FileNotFoundError:
             return 404, "not found"
+            
+    def rse_statistics_data(self, request, rse=None, **args):
+        runs = self.App.WMDataSource.all_stats_for_rse(rse)
+        # filter out all errors
+        runs = [r for r in runs if r.get("status") == "done" and not r["error"]]
+        return json.dumps(runs), "text/json"
         
     #
     # GUI
     #
         
     def index(self, request, relpath, **args):
-        data = self.App.WMDataSource.stats()
+        data = self.App.WMDataSource.latest_stat_per_rse()
         rses = sorted(list(data.keys()))
         return self.render_to_response("wm_index.html", rses = rses, data=data)
         
     def rse(self, request, relpath, rse=None, **args):
         if not rse:
             return "RSE must be specified", 400
-        try:    data = self.App.WMDataSource.stats_for_rse(rse)
-        except JSONParseError as e:
-            return str(e), 500
-        if data is None:
-            return f"RSE {rse} not found", 404
-        return self.render_to_response("wm_rse.html", rse=rse, data=data)
+
+        stats_by_run = self.App.WMDataSource.all_stats_for_rse(rse)
+        if not stats_by_run:
+            return f"Data for RSE {rse} not found", 404
+        latest_run = stats_by_run[-1]
+            
+        # filter out all errors
+        stats_by_run = [r for r in stats_by_run if r.get("status") == "done" and not r["error"] and r.get("start_time") and r.get("end_time")]
+        for r in stats_by_run:
+            r["elapsed_time"] = (r["end_time"] - r["start_time"])/3600
+            r["start_time_miliseconds"] = int(r["start_time"]*1000)
+        return self.render_to_response("wm_rse.html", rse=rse, latest_run=latest_run, stats_by_run=stats_by_run)
         
     def ls(self, request, relpath, rse=None, **args):
         lst = self.App.WMDataSource.ls(rse)
