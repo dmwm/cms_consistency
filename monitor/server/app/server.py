f0c63d32343a4ee293ac767aea5cbd8e688ce73d
Igor Mandrichenko <igorvm@gmail.com>
2021-08-02 15:38:12 -0500

    .

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -72,9 +72,9 @@ class Handler(WPHandler):
         um_stats = um_data_source.latest_stats_per_rse()
         um_summaries = {rse: um_data_source.run_summary(stats) for rse, stats in um_stats.items()}
         
-        print("um_summaries:")
-        for rse, summary in um_summaries.items():
-            print (rse, summary)
+        print("um_stats:")
+        for rse, stats in um_stats.items():
+            print (rse, stats)
         sys.stdout.flush()
         
         all_rses = set(cc_stats.keys()) | set(um_stats.keys())
