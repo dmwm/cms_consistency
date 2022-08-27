323eb397eeaad255486ebc02bd8edd0b7057e4ee
Igor Mandrichenko <ivm@fnal.gov>
2022-08-27 08:00:36 -0400

    .

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from wm_handler import WMHandler, UMDataSource
 from data_source import CCDataSource, UMDataSource
 
-Version = "1.11.7"
+Version = "1.11.8"
 
 def display_file_list(lst):
     Indent = "    "
