cbc0a7a5e38d4b2a8f93c3ffdb3bbbeb2bb3d138
Igor Mandrichenko <ivm@fnal.gov>
2022-08-27 07:12:26 -0400

    .

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -80,11 +80,11 @@ class Handler(WPHandler):
         elif sort == "cc_run":
             infos = sorted(infos, key=lambda x: ((x["cc_summary"] or {}).get("start_time", -1), x["rse"]))
         elif sort == "-cc_run":
-            infos = sorted(infos, key=lambda x: (-(x["cc_summary"] or {}).get("start_time", -1), x["rse"]))
+            infos = sorted(infos, key=lambda x: ((x["cc_summary"] or {}).get("start_time", -1), x["rse"]), reverse=True)
         elif sort == "um_run":
             infos = sorted(infos, key=lambda x: ((x["um_summary"] or {}).get("start_time", -1), x["rse"]))
         elif sort == "-um_run":
-            infos = sorted(infos, key=lambda x: (-(x["um_summary"] or {}).get("start_time", -1), x["rse"]))
+            infos = sorted(infos, key=lambda x: ((x["um_summary"] or {}).get("start_time", -1), x["rse"]), reverse=True)
         
         return self.render_to_response("rses_combined.html", infos=infos)
         
