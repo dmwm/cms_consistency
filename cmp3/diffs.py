b04aebf917c63b8f96237459fbb63825204a5e1a
Igor Mandrichenko <ivm@fnal.gov>
2022-08-29 09:50:11 -0400

    .

--- cmp3/diffs.py
+++ cmp3/diffs.py
@@ -14,22 +14,22 @@ def diff(prev, last):
     return old_set, new_set
 
 Usage = """
-python diffs.py [-s <stats.json>] [-S <section key>] [-j] <storage path> <RSE> [<run id>]
-    -p - print results to stdout
-        -j - print results as JSON
-    -u - update run stats in place
-    -s - save results into JSON stats file
-    -S - section key for the stats file, used with -s or -u. Default: "diffs"
+python diffs.py [options] <storage path> <RSE> [<run id>]
+    -p                      - print results to stdout
+        -j                  - if -p is used, print results as JSON
+    -u                      - update run stats in place
+    -s <JSON file>          - save results into JSON stats file
+    -S                      - section key for the stats file, used with -s or -u. Default: "diffs"
 """
 
-opts, args = getopt.getopt(sys.argv[1:], "s:S:j")
+opts, args = getopt.getopt(sys.argv[1:], "s:S:jpu")
 opts = dict(opts)
 
 if not args:
     print(Usage)
     sys.exit(2)
 
-section_key = opts.get("-S", section_key)
+section_key = opts.get("-S", "diffs")
 
 as_json = "-j" in opts
 
@@ -54,6 +54,7 @@ old_dark, new_dark = diff(prev_run.dark_files(), run.dark_files())
 old_missing, new_missing = diff(prev_run.missing_files(), run.missing_files())
 
 diff_data = dict(
+        run=run.Run,
         prev_run=prev_run.Run,
         nmissing_old=len(old_missing), 
         nmissing_new=len(new_missing), 
@@ -62,11 +63,11 @@ diff_data = dict(
 )
 
 if "-p" in opts:
-    if "-j" in opts:
+    if as_json:
         print(json.dumps(diff_data, indent=4, sort_keys=True))
     else:
-        for k, v in sorted(diff_data.items):
-            print("{k}: {v}")
+        for k, v in sorted(diff_data.items()):
+            print(f"{k}: {v}")
 
 if "-u" in opts:
     stats = Stats(run.stats_path())
