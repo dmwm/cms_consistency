fb37d6c31b8e48a2306a5a80f5a84a60d67d1115
Igor Mandrichenko <ivm@fnal.gov>
2022-08-30 15:58:37 -0400

    .

--- cmp3/db_list.py
+++ cmp3/db_list.py
@@ -96,42 +96,41 @@ if "-c" in opts:
 else:
     dbconfig = DBConfig.from_cfg(opts["-d"])
 
-try:
-    Base = declarative_base()
-    if dbconfig.Schema:
-        Base.metadata.schema = dbconfig.Schema
+Base = declarative_base()
+if dbconfig.Schema:
+    Base.metadata.schema = dbconfig.Schema
 
-    class Replica(Base):
-            __tablename__ = "replicas"
-            path = Column(String)
-            state = Column(String)
-            rse_id = Column(GUID(), primary_key=True)
-            scope = Column(String, primary_key=True)
-            name = Column(String, primary_key=True)
+class Replica(Base):
+        __tablename__ = "replicas"
+        path = Column(String)
+        state = Column(String)
+        rse_id = Column(GUID(), primary_key=True)
+        scope = Column(String, primary_key=True)
+        name = Column(String, primary_key=True)
 
-    class RSE(Base):
-            __tablename__ = "rses"
-            id = Column(GUID(), primary_key=True)
-            rse = Column(String)
+class RSE(Base):
+        __tablename__ = "rses"
+        id = Column(GUID(), primary_key=True)
+        rse = Column(String)
 
-    engine = create_engine(dbconfig.DBURL,  echo=verbose)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+engine = create_engine(dbconfig.DBURL,  echo=verbose)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    rse = session.query(RSE).filter(RSE.rse == rse_name).first()
-    if rse is None:
-            print ("RSE %s not found" % (rse_name,))
-            sys.exit(1)
+rse = session.query(RSE).filter(RSE.rse == rse_name).first()
+if rse is None:
+        print ("RSE %s not found" % (rse_name,))
+        sys.exit(1)
 
-    replicas = session.query(Replica).filter(Replica.rse_id==rse.id)
-    if include_states != '*':
-        replicas = replicas.filter(Replica.state.in_(list(include_states)))
-    if exclude_states:
-        replicas = replicas.filter(Replica.state.not_in(list(exclude_states)))
+replicas = session.query(Replica).filter(Replica.rse_id==rse.id)
+if include_states != '*':
+    replicas = replicas.filter(Replica.state.in_(list(include_states)))
+if exclude_states:
+    replicas = replicas.filter(Replica.state.not_in(list(exclude_states)))
 
-    for r in replicas.yield_per(10000):
-        path = r.name
-        state = r.state
-        name = r.name
-        scope = r.scope
-        print(stats, name, path or "")
+for r in replicas.yield_per(10000):
+    path = r.name
+    state = r.state
+    name = r.name
+    scope = r.scope
+    print(stats, name, path or "")
