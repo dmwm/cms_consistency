4e2971f66ce766c673b2ea3ec305966200dac097
Igor Mandrichenko <ivm@fnal.gov>
2022-09-12 10:55:02 -0400

    .

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -5,7 +5,7 @@ from um_handler import UMHandler
 from ce_handler import CEHandler
 from data_source import CCDataSource, UMDataSource, StatsCache
 
-Version = "2.0.2"
+Version = "2.1.0"
 
 def display_file_list(lst):
     Indent = "    "
