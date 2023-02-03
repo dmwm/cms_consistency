71bc360f869b4cf4471eecd0080afc90b6dc7018
Igor Mandrichenko <ivm@fnal.gov>
2023-02-03 10:03:54 -0500

    Move config file copying to the outside script

--- site_cmp3/site_cmp3.sh
+++ site_cmp3/site_cmp3.sh
@@ -28,13 +28,6 @@ echo "out:                       $out"
 echo "cert:                      $cert"
 echo "key:                       $key"
 
-if [ ! -f /consistency/config.yaml ]; then
-    cp $config_file /consistency/config.yaml    # to make it editable
-    echo Config file $config_file copied to /consistency/config.yaml
-fi
-
-config_file=/consistency/config.yaml
-
 python=${PYTHON:-python}
 
 export PYTHONPATH=`pwd`:`pwd`/cmp3
