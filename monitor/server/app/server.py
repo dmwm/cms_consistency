540522046df078a8920be0d08b88859d2a478979
Igor Mandrichenko <igorvm@gmail.com>
2023-06-27 11:06:02 -0500

    .

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -5,7 +5,7 @@ from um_handler import UMHandler
 from ce_handler import CEHandler
 from data_source import CCDataSource, UMDataSource, StatsCache
 
-Version = "2.3.10"
+Version = "2.3.11"
 
 def display_file_list(lst):
     Indent = "    "
