737fde974810b1199f9988917f4131f856931d37
Igor Mandrichenko <igorvm@gmail.com>
2023-08-10 07:31:51 -0500

    Version bump

--- monitor/server/app/server.py
+++ monitor/server/app/server.py
@@ -5,7 +5,7 @@ from um_handler import UMHandler
 from ce_handler import CEHandler
 from data_source import CCDataSource, UMDataSource, StatsCache
 
-Version = "2.3.12"
+Version = "2.4.0"
 
 def display_file_list(lst):
     Indent = "    "
