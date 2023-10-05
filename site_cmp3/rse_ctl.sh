299a3aaebb531e13e8c57c5642ec6f74cbc36dbb
ivm <ivm@fnal.gov>
2023-10-05 09:08:05 -0500

    Added rse_ctl.sh

---
+++ site_cmp3/rse_ctl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+action=$1
+rse=$2
+
+case $action in
+disable)
+	rucio-admin rse set-attribute --rse $rse --key CE_config.ce_disabled --value true
+  ;;
+enable)
+	rucio-admin rse set-attribute --rse $rse --key CE_config.ce_disabled --value false
+  ;;
+show)
+  rucio-admin rse get-attribute $rse | grep ^CE_config.ce_disabled:
+fi
