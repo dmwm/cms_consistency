9bab3284d62907e6db83e6093683a86d8544a6c3
Guilherme Lima <lima@fnal.gov>
2026-03-18 12:27:06 -0500

    Document rucioconmon:2.0.1

--- monitor/server/build.sh
+++ monitor/server/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export MONITOR_VERSION=rucio-con-mon:2.0.0
+export MONITOR_VERSION=rucio-con-mon:2.0.1
 export HARBOR=registry.cern.ch/cmsrucio
 
 podman build -t $HARBOR/$MONITOR_VERSION .
--- monitor/server/start.sh
+++ monitor/server/start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo start.sh: version 2
+echo start.sh: version 2.0.1
 
 CC_DATA=/reports
 WM_DATA=/reports/unmerged
 PATH=/app/.venv/bin:$PATH
 
-echo "--- starting ---"
-cd /app
-
 python -V
+cd /app
 
 echo "--- starting server with: " python app/server.py --um-ignore /store/unmerged/logs/ -p 8400 "$@" $CC_DATA $WM_DATA
 python app/server.py --um-ignore /store/unmerged/logs/ -p 8400 "$@" $CC_DATA $WM_DATA
